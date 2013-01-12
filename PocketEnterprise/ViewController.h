52ca81b91e6629b505bbdb2a81a4f161ba38a29a
Kiran Soumya <kiran.soumya@gmail.com>
2013-01-12 17:01:37 +0530

    Login works on view controller removed table view

--- PocketEnterprise/ViewController.h
+++ PocketEnterprise/ViewController.h
@@ -7,7 +7,13 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <RestKit/RestKit.h>
 
 @interface ViewController : UIViewController
+
+@property (weak, nonatomic) IBOutlet UITextField *Email;
+@property (weak, nonatomic) IBOutlet UITextField *Password;
+
+- (IBAction)LoginButtonPressed:(id)sender;
 
 @end
--- PocketEnterprise/ViewController.m
+++ PocketEnterprise/ViewController.m
@@ -13,6 +13,8 @@
 @end
 
 @implementation ViewController
+@synthesize Email;
+@synthesize Password;
 
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
 {
@@ -35,4 +37,38 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (IBAction)LoginButtonPressed:(id)sender {
+    //Authentication token for every request devise
+    /*NSString *path = [NSString stringWithFormat:@"/%@/%i", [self pluralizedName], identifier];
+     path = [path stringByAppendingQueryParameters:[self appendAuthenticationToken:params]];
+     [[RKObjectManager sharedManager] loadObjectsAtResourcePath:path delegate:delegate];*/
+    
+    NSLog(@"Log In button was pressed!");
+    NSLog(@"%@",Email.text);
+    
+    NSDictionary *params = [NSDictionary dictionaryWithObjects:[NSArray arrayWithObjects:
+                                                                Email.text,
+                                                                Password.text,
+                                                                Password.text,
+                                                                nil]
+                                                       forKeys:[NSArray arrayWithObjects:
+                                                                @"email",
+                                                                @"password",
+                                                                @"password_confirmation",
+                                                                nil]];
+    
+    NSURL* url = [[NSURL alloc]initWithString:@"http://localhost:3000"];
+    RKObjectManager* objectManager = [RKObjectManager managerWithBaseURL:url];
+    
+    [objectManager.HTTPClient setAuthorizationHeaderWithUsername:Email.text password:Password.text];
+    
+    [objectManager getObjectsAtPath:@"/welcome/index.json" parameters:nil success:^(RKObjectRequestOperation *operation, RKMappingResult *mappingResult)
+     {
+         NSLog(@"It Worked: %@", [mappingResult array]);
+         // Or if you're only expecting a single object:
+         NSLog(@"It Worked: %@", [mappingResult firstObject]);
+     } failure:^(RKObjectRequestOperation *operation, NSError *error) {
+         NSLog(@"It Failed: %@", error);
+     }];
+}
 @end
